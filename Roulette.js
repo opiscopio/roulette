eb6c1a627e66484a6c1db2307598d4ab76d22237
JorensM <jorensmerenjanu@gmail.com>
2024-12-07 14:11:58 +0200

    feat: add graphics

--- Roulette.js
+++ Roulette.js
@@ -20,6 +20,9 @@ class RouletteBetButton {
      * @type { HTMLElement }
      */
     chipElement;
+    chipImage = document.createElement('img');
+    chipText = document.createElement('span');
+
 
     rotation;
 
@@ -39,6 +42,13 @@ class RouletteBetButton {
         this.chipElement.style.alignItems = 'center';
         this.chipElement.style.justifyContent = 'center';
         this.chipElement.classList.add('chip');
+
+        this.chipImage.classList.add('table-chip-img')
+
+        this.chipText.classList.add('table-chip-text');
+        
+        this.chipElement.append(this.chipImage);
+        this.chipElement.append(this.chipText);
     }
 
     addBet(amount) {
@@ -54,7 +64,7 @@ class RouletteBetButton {
         if(this.bets.length > 0) {
             this.chipElement.style.display = 'flex';
             this.chipElement.style.position = 'absolute';
-            this.chipElement.innerHTML = this.bets.reduce((prev, curr) => prev + curr).toString();
+            this.chipText.innerHTML = this.bets.reduce((prev, curr) => prev + curr).toString();
             const buttonPosition = this.button.getBoundingClientRect();
             this.chipElement.style.left = buttonPosition.x + 
                 (this.rotation ? this.button.clientHeight : this.button.clientWidth) / 
@@ -62,12 +72,24 @@ class RouletteBetButton {
             this.chipElement.style.top = buttonPosition.y + 
                 (this.rotation ? this.button.clientWidth : this.button.clientHeight) / 
                 2 + 1 + 'px';
-            this.chipElement.style.backgroundColor = 'green';
-            this.chipElement.style.borderColor = 'white';
-            this.chipElement.style.borderWidth = '4px';
-            this.chipElement.style.borderStyle = 'dashed';
+            // this.chipElement.style.backgroundColor = 'green';
+            // this.chipElement.style.borderColor = 'white';
+            // this.chipElement.style.borderWidth = '4px';
+            // this.chipElement.style.borderStyle = 'dashed';
             this.chipElement.style.width = '48px';
             this.chipElement.style.height = '48px';
+            const TOTAL_BET = this.getTotalBetAmount();
+            if(TOTAL_BET >= 1000) {
+                this.chipImage.src = './res/ChipOrange.svg';
+            } else if(TOTAL_BET >= 500) {
+                this.chipImage.src = './res/ChipBlueDark.svg';
+            } else if(TOTAL_BET >= 200) {
+                this.chipImage.src = './res/ChipRed.svg';
+            } else if(TOTAL_BET >= 100){
+                this.chipImage.src = './res/ChipGreen.svg';
+            } else {
+                this.chipImage.src = './res/ChipBlueLight.svg';
+            }
             // this.chipElement.style.opacity = '100%';
         } else {
             this.chipElement.style.display = 'none';
